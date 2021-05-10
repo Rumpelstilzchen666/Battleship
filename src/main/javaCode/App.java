a4f150ea4ec88dbc0cec67e2aeb2b52620e060a1
Rumpelstilzchen <Rumpelstilzchen2018@yandex.ru>
2021-05-11 02:05:15 +0300

    GUI for main menu

--- src/main/javaCode/App.java
+++ src/main/javaCode/App.java
@@ -1,23 +1,105 @@
 package javaCode;
 
-public class App {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+import static javaCode.AppUI.*;
+
+public class App extends Application {
+    private static final HashMap<String, ShipType[]> shipTypes = new HashMap<>();
+    private Stage primaryStage;
+    private Battle battle;
+
+    public App() {
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Морской бой");
+        this.primaryStage.getIcons().add(
+                new Image(Objects.requireNonNull(getClass().getResourceAsStream("/resources/icon.jpg"))));
+        this.primaryStage.setScene(getMainMenuScene());
+        //this.primaryStage.setFullScreen(true);
+        this.primaryStage.show();
+    }
+
+    private Scene getMainMenuScene() {
+        final Label mainLabel = new Label("Морской бой");
+        mainLabel.setId("main-label");
+        final GridPane mainMenuGrid = getMineMenuGrid();
+        mainMenuGrid.setAlignment(Pos.TOP_CENTER);
+        final VBox vBox = new VBox(mainLabel, mainMenuGrid);
+        vBox.setAlignment(Pos.TOP_CENTER);
+        vBox.setId("main-menu");
+        final Scene mainMenuScene = new Scene(vBox);
+        mainMenuScene.getStylesheets().add(
+                Objects.requireNonNull(getClass().getResource("/resources/styles.css")).toExternalForm());
+        return mainMenuScene;
+    }
+
+    private GridPane getMineMenuGrid() {
+        final String[] buttonLabels = new String[] {"Играть", "Настройки", "Правила", "Выход"};
+        final int buttonWidthInCells = 3;
+        final int nCols = buttonWidthInCells + 2;
+        final int cellSize = 100;
+        final GridPane grid = getBattleGrid(buttonLabels.length, nCols, cellSize);
+
+        final double shipWidth = getWidth(getShipPolygon(buttonWidthInCells, cellSize)),
+                shipHeight = getHeight(getShipPolygon(buttonWidthInCells, cellSize));
+        for(int rowN = 0; rowN < buttonLabels.length; rowN++) {
+            grid.add(getShipPolygon(buttonWidthInCells, cellSize),
+                    (nCols - buttonWidthInCells) / 2 + 1, rowN + 1, buttonWidthInCells, 1);
+
+            Button button = new Button(buttonLabels[rowN]);
+            button.getStyleClass().add("main-menu-button");
+            button.setStyle("-fx-font-size: " + cellSize / 4 + ';');
+            button.setPrefSize(shipWidth, shipHeight);
+            switch(rowN) {
+                case 0 -> button.setOnAction(actionEvent -> startBattle());
+                case 3 -> button.setOnAction(actionEvent -> System.exit(0));
+                default -> button.setOnAction(null);
+            }
+            grid.add(button, (nCols - buttonWidthInCells) / 2 + 1, rowN + 1, buttonWidthInCells, 1);
+        }
+        return grid;
+    }
+
+    private void startBattle() {
+        battle = new Battle(10, shipTypes.get("ruWiki"));
+        System.out.println("Игра началась!");
+    }
+
     public static void main(String[] args) {
-        ShipType[] ruWiki = new ShipType[] {
+        Application.launch();
+        shipTypes.put("ruWiki", new ShipType[] {
                 new ShipType("Линкор", 1, 4),
                 new ShipType("Крейсер", 2, 3),
                 new ShipType("Эсминец", 3, 2),
-                new ShipType("Торпедный катер", 4, 1)};
-        ShipType[] Hasbro = new ShipType[] {
+                new ShipType("Торпедный катер", 4, 1)
+        });
+        shipTypes.put("Hasbro", new ShipType[] {
                 new ShipType("Авианосец", 1, 5),
                 new ShipType("Линкор", 1, 4),
                 new ShipType("Эсминец", 1, 3),
                 new ShipType("Подлодка", 1, 3),
-                new ShipType("Сторожевой корабль", 1, 2)};
-        ShipType[] mine = new ShipType[] {
+                new ShipType("Сторожевой корабль", 1, 2)
+        });
+        shipTypes.put("mine", new ShipType[] {
                 new ShipType("Авианосец", 1, 4),
                 new ShipType("Крейсер", 1, 3),
                 new ShipType("Подлодка", 1, 3),
-                new ShipType("Ракетный катер", 4, 2)};
-        Battle battle = new Battle(10, ruWiki);
+                new ShipType("Ракетный катер", 4, 2)
+        });
     }
 }
---
+++ src/main/javaCode/AppUI.java
@@ -0,0 +1,101 @@
+package javaCode;
+
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.shape.Polygon;
+
+import java.util.Arrays;
+
+public class AppUI {
+    public static GridPane getBattleGrid(final int size, final int cellSize) {
+        return getBattleGrid(size, size, cellSize);
+    }
+
+    public static GridPane getBattleGrid(final int nRows, final int nCols, final int cellSize) {
+        final GridPane grid = new GridPane();
+        grid.getStyleClass().add("grid");
+        grid.setGridLinesVisible(true);
+        final double width = cellSize * (nCols + 1), height = cellSize * (nRows + 1);
+        grid.setMinSize(width, height);
+        grid.setPrefSize(width, height);
+        grid.setMaxSize(width, height);
+        grid.getColumnConstraints().addAll(getColumnConstraintsForGrid(nCols + 1));
+        grid.getRowConstraints().addAll(getRowConstraintsForGrid(nRows + 1));
+
+        for(int i = 1; i < nCols + 1; i++) {
+            grid.add(getLabelForGrid((char) ('А' + i - 1) + "", cellSize), i, 0);
+        }
+        for(int i = 1; i < nRows + 1; i++) {
+            grid.add(getLabelForGrid(i + "", cellSize), 0, i);
+        }
+
+        final GridPane gridBattlefield = new GridPane();
+        gridBattlefield.getStyleClass().addAll("grid-battlefield", "grid");
+        gridBattlefield.setGridLinesVisible(true);
+        gridBattlefield.getColumnConstraints().addAll(getColumnConstraintsForGrid(nCols));
+        gridBattlefield.getRowConstraints().addAll(getRowConstraintsForGrid(nRows));
+        grid.add(gridBattlefield, 1, 1, nCols, nRows);
+        return grid;
+    }
+
+    private static ColumnConstraints[] getColumnConstraintsForGrid(final int nCols) {
+        ColumnConstraints[] columnConstraints = new ColumnConstraints[nCols];
+        for(int colN = 0; colN < nCols; colN++) {
+            columnConstraints[colN] = new ColumnConstraints();
+            columnConstraints[colN].setPercentWidth(100);
+            columnConstraints[colN].setHalignment(HPos.CENTER);
+        }
+        return columnConstraints;
+    }
+
+    private static RowConstraints[] getRowConstraintsForGrid(final int nRows) {
+        RowConstraints[] rowConstraints = new RowConstraints[nRows];
+        for(int rowN = 0; rowN < nRows; rowN++) {
+            rowConstraints[rowN] = new RowConstraints();
+            rowConstraints[rowN].setPercentHeight(100);
+            rowConstraints[rowN].setValignment(VPos.CENTER);
+        }
+        return rowConstraints;
+    }
+
+    private static Label getLabelForGrid(final String text, final int cellSize) {
+        Label label = new Label(text);
+        label.getStyleClass().add("grid-label");
+        label.setStyle("-fx-font-size: " + cellSize / 3 + ';');
+        return label;
+    }
+
+    public static Polygon getShipPolygon(final int widthInCells, final int cellSize) {
+        final double padding = cellSize * 0.1;
+        final double shipLen = cellSize * widthInCells - padding * 2;
+        final double shipWidth = cellSize - padding * 2;
+        Polygon ship =
+                new Polygon(0, 0, shipLen * 3 / 4, 0, shipLen, shipWidth / 2, shipLen * 3 / 4, shipWidth, 0, shipWidth);
+        ship.getStyleClass().add("ship");
+        return ship;
+    }
+
+    public static double getHeight(Polygon polygon) {
+        final Double[] pointsCoordinates = polygon.getPoints().toArray(new Double[0]);
+        final int nPoints = pointsCoordinates.length / 2;
+        final double[] pointsY = new double[nPoints];
+        for(int i = 0; i < nPoints; i++) {
+            pointsY[i] = pointsCoordinates[2 * i + 1];
+        }
+        return Arrays.stream(pointsY).max().getAsDouble() - Arrays.stream(pointsY).min().getAsDouble();
+    }
+
+    public static double getWidth(Polygon polygon) {
+        final Double[] pointsCoordinates = polygon.getPoints().toArray(new Double[0]);
+        final int nPoints = pointsCoordinates.length / 2;
+        final double[] pointsX = new double[nPoints];
+        for(int i = 0; i < nPoints; i++) {
+            pointsX[i] = pointsCoordinates[2 * i];
+        }
+        return Arrays.stream(pointsX).max().getAsDouble() - Arrays.stream(pointsX).min().getAsDouble();
+    }
+}
